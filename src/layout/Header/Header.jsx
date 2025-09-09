169fdb91bb25982dcd500a838c4850256480e7bf
gosmdochi <nohys0312@gmail.com>
2025-09-09 20:27:59 +0900

    헤더 수정

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -6,7 +6,7 @@ import Login from "@/shared/ui/Login";
 
 const Header = () => {
     return (
-        <header className = "flex justify-between w-full h-21.6 items-center">
+        <header className = "mx-4 flex justify-between h-21.6 items-center">
             <Link to={`/`}>
                 <Logo className = "flex w-26 cursor-pointer justify-start"/>
             </Link>
